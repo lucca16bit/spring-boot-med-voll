ea89707bf858de62288baa5019f704b0420a6dff
lucas <lucaston2323@gmail.com>
2024-12-30 17:00:31 -0300

    feat: repository para o médico

--- src/main/java/com/med/voll/api/controller/MedicoController.java
+++ src/main/java/com/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,10 @@
 package com.med.voll.api.controller;
 
 import com.med.voll.api.dto.medico.DadosCadastroMedicoDto;
+import com.med.voll.api.models.Medico;
+import com.med.voll.api.repository.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository repository;
+
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroMedicoDto dados) { // o requestbody vai salvar o corpo da json nesta string
-        System.out.println(dados);
+    @Transactional
+    public void cadastrar(@RequestBody DadosCadastroMedicoDto dados) { // o requestbody vai salvar o corpo da json nesta variavel
+        repository.save(new Medico(dados));
     }
 }
--- src/main/java/com/med/voll/api/dto/medico/DadosCadastroMedicoDto.java
+++ src/main/java/com/med/voll/api/dto/medico/DadosCadastroMedicoDto.java
@@ -1,6 +1,7 @@
 package com.med.voll.api.dto.medico;
 
 import com.med.voll.api.dto.endereco.DadosEnderecoDto;
+import com.med.voll.api.models.Especialidade;
 
 public record DadosCadastroMedicoDto(
         String nome,
