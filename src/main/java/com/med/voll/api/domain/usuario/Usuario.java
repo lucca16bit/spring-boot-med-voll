60435aaf0059f0b59332a70b5837c56ca7a84ddb
lucas <lucaston2323@gmail.com>
2025-01-06 15:49:03 -0300

    feat: entidade usuario para autenticaçao da API

---
+++ src/main/java/com/med/voll/api/domain/usuario/Usuario.java
@@ -0,0 +1,37 @@
+package com.med.voll.api.domain.usuario;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+
+@Table(name = "usuarios")
+@Entity(name = "usuario")
+
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+
+    public Usuario() {}
+
+    public Usuario(Long id, String login, String senha) {
+        this.id = id;
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+}
---
+++ src/main/java/com/med/voll/api/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.med.voll.api.repository;
+
+import com.med.voll.api.domain.usuario.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    UserDetails findByLogin(String login);
+}
