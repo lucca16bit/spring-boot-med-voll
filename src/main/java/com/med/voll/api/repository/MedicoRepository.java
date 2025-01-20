b391f438e056e90baf87cd996a04b2e18d5d8971
lucas <lucaston2323@gmail.com>
2025-01-20 14:52:39 -0300

    fix: pequeno detalhe na função random, onde eu utilizava a funçao do mysql ao invés do postgresql

--- src/main/java/com/med/voll/api/repository/MedicoRepository.java
+++ src/main/java/com/med/voll/api/repository/MedicoRepository.java
@@ -26,7 +26,7 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
                 and
                 c.motivoCancelamento is null
             )
-            order by rand()
+            order by random()
             limit 1
     """)
     Medico escolherMedicoAleatorioLivreNaData(Especialidade especialidade, LocalDateTime data);
